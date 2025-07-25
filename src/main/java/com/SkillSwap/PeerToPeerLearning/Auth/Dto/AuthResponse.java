8216feb7f29fb43c440d5865fff8df9210161536
Shanatanu Kale <kaleshantanu2260@gmail.com>
2025-07-25 18:38:37 +0530

    Updated Auth module with new classes and modifications

--- src/main/java/com/SkillSwap/PeerToPeerLearning/Auth/Dto/AuthResponse.java
+++ src/main/java/com/SkillSwap/PeerToPeerLearning/Auth/Dto/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.SkillSwap.PeerToPeerLearning.Auth.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
+    private String email;
+    private String token;
 }
